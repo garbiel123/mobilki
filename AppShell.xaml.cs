705b0978b93c1d939f715c34732c1b892aceae19
Gabriel Odbierzychleb <gabodbi@gmail.com>
2025-09-15 09:20:32 +0200

    moblikimango

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace mobilkimango
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "mobilkimango")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "mobilkimango.Pages")]
